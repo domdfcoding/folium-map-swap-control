ff367ecda297b788db3e3eb8dbb023bbb36e06dc
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-05-14 08:44:42 +0100

    Bump fontawesome to latest 6.x

--- folium_map_swap_control/__init__.py
+++ folium_map_swap_control/__init__.py
@@ -69,7 +69,7 @@ class MapSwapControl(folium.elements.JSCSSMixin, folium.elements.MacroElement): 
 	default_css = [
 			(
 					"fontawesome_css",
-					"https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/all.min.css",
+					"https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.7.2/css/all.min.css",
 					),
 			(
 					"map_swap_control_css",
